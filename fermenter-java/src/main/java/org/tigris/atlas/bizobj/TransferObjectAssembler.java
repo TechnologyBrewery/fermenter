2d0e2a6ed514736357e08da732909d21553e9555
Ryan Ashcraft <d.ryan.ashcraft@gmail.com>
2014-03-15 10:42:51 -0400

    Ease Assembler Usage: updated to make all method access on assemblers static so we don't have to create useless instances of the assemblers all over the place

--- fermenter-java/src/main/java/org/tigris/atlas/bizobj/TransferObjectAssembler.java
+++ fermenter-java/src/main/java/org/tigris/atlas/bizobj/TransferObjectAssembler.java
@@ -4,27 +4,24 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tigris.atlas.transfer.TransferObject;
 
 /**
  * Provides default transfer object assembler behavior.
  */
 public abstract class TransferObjectAssembler {
-
-	/**
-	 * Returns the subclasses {@link Logger} instance to allow for more granular logging.
-	 * @return {@link Logger}
-	 */
-	protected abstract Logger getLog();
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TransferObjectAssembler.class);
+	
 	/**
 	 * Sets oplock on the the transfer object.
 	 * @param to The object on which the oplock should be set
 	 * @param oplockValue The oplock value to set
 	 */
-	protected void setOplockOnTransferObject(TransferObject to, Integer oplockValue) {
-		Class toClass = to.getClass();
-		Class[] paramClasses  = {Integer.class};
+	protected static void setOplockOnTransferObject(TransferObject to, Integer oplockValue) {
+		Class<?> toClass = to.getClass();
+		Class<?>[] paramClasses  = {Integer.class};
 		Object[] paramInstanes = {oplockValue};
 
 		try {
@@ -32,15 +29,15 @@ public abstract class TransferObjectAssembler {
 			m.setAccessible(true);
 			m.invoke(to, paramInstanes);
 		} catch (SecurityException e) {
-			getLog().error("A security exception was encountered while setting oplock!", e);
+			LOGGER.error("A security exception was encountered while setting oplock!", e);
 		} catch (IllegalArgumentException e) {
-			getLog().error("An illegal argument exception was encountered while setting oplock!", e);
+			LOGGER.error("An illegal argument exception was encountered while setting oplock!", e);
 		} catch (NoSuchMethodException e) {
-			getLog().error("Setter method exception was encountered while setting oplock!", e);
+			LOGGER.error("Setter method exception was encountered while setting oplock!", e);
 		} catch (IllegalAccessException e) {
-			getLog().error("An illegal access exception was encountered while setting oplock!", e);
+			LOGGER.error("An illegal access exception was encountered while setting oplock!", e);
 		} catch (InvocationTargetException e) {
-			getLog().error("An invocationx exception was encountered while setting oplock!", e);
+			LOGGER.error("An invocation exception was encountered while setting oplock!", e);
 		}
 	}
 

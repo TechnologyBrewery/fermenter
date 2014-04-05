c506ec96343bc464265012068e7f6f71616e8fb9
Ryan Ashcraft <d.ryan.ashcraft@gmail.com>
2014-04-05 12:06:14 -0400

    Defect: parent references are not merged from TO -> BO when assembled

--- fermenter-test/fermenter-test-domain-integration-test/src/test/java/com/ask/test/domain/service/TestUtils.java
+++ fermenter-test/fermenter-test-domain-integration-test/src/test/java/com/ask/test/domain/service/TestUtils.java
@@ -8,6 +8,10 @@ import java.util.Date;
 
 import org.apache.commons.lang.math.RandomUtils;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tigris.atlas.messages.Message;
+import org.tigris.atlas.messages.MessageUtils;
 import org.tigris.atlas.messages.Messages;
 import org.tigris.atlas.service.ServiceResponse;
 
@@ -21,48 +25,56 @@ import com.ask.test.domain.transfer.ValidationExampleChild;
  * Contains common integration test logic for this project.
  */
 public final class TestUtils {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger("TestUtils");
 
-    public static final String DOMAIN_GROUPID_ARTIFACTID_VERSION = "com.ask.fermenter:fermenter-test-domain:1-SNAPSHOT";
+	public static final String DOMAIN_GROUPID_ARTIFACTID_VERSION = "com.ask.fermenter:fermenter-test-domain:1-SNAPSHOT";
 
-    private TestUtils() {
-	// prevent instantiation
-    }
-
-    public static SimpleDomain createRandomSimpleDomain() {
-	SimpleDomain domain = TransferObjectFactory.createSimpleDomain();
-	domain.setName(RandomStringUtils.randomAlphanumeric(25));
-	domain.setTheDate1(new Date());
-	domain.setTheLong1(RandomUtils.nextLong());
-	domain.setType(RandomStringUtils.random(5));
-	domain.setAnEnumeratedValue(SimpleDomainEnumeration.values()[RandomUtils.nextInt(SimpleDomainEnumeration
-		.values().length)]);
-	return domain;
-    }
-
-    public static ValidationExample createRandomValidationExample() {
-	ValidationExample domain = TransferObjectFactory.createValidationExample();
-	domain.setRequiredField(RandomStringUtils.randomAlphabetic(20));
-	return domain;
-    }
-
-    public static ValidationExampleChild createRandomValidationExampleChild() {
-	ValidationExampleChild domain = TransferObjectFactory.createValidationExampleChild();
-	domain.setRequiredField(RandomStringUtils.randomAlphabetic(20));
-	return domain;
-    }
-
-    public static void assertNoErrorMessages(ServiceResponse response) {
-	if (response != null) {
-	    Messages messages = response.getMessages();
-	    assertFalse(messages.hasErrorMessages());
+	private TestUtils() {
+		// prevent instantiation
 	}
-    }
 
-    public static void assertErrorMessagesInResponse(ServiceResponse response, int expectedNumErrorMessages) {
-	assertNotNull("Service response wrapper was unexpectedly null", response);
-	Messages messages = response.getMessages();
-	assertNotNull("Messages object on service response wrapper was unexpected null", messages);
-	assertEquals("An unexpected number of error messages were found", expectedNumErrorMessages,
-		messages.getErrorMessageCount());
-    }
+	public static SimpleDomain createRandomSimpleDomain() {
+		SimpleDomain domain = TransferObjectFactory.createSimpleDomain();
+		domain.setName(RandomStringUtils.randomAlphanumeric(25));
+		domain.setTheDate1(new Date());
+		domain.setTheLong1(RandomUtils.nextLong());
+		domain.setType(RandomStringUtils.random(5));
+		domain.setAnEnumeratedValue(SimpleDomainEnumeration.values()[RandomUtils.nextInt(SimpleDomainEnumeration
+				.values().length)]);
+		return domain;
+	}
+
+	public static ValidationExample createRandomValidationExample() {
+		ValidationExample domain = TransferObjectFactory.createValidationExample();
+		domain.setRequiredField(RandomStringUtils.randomAlphabetic(20));
+		return domain;
+	}
+
+	public static ValidationExampleChild createRandomValidationExampleChild() {
+		ValidationExampleChild domain = TransferObjectFactory.createValidationExampleChild();
+		domain.setRequiredField(RandomStringUtils.randomAlphabetic(20));
+		return domain;
+	}
+
+	public static void assertNoErrorMessages(ServiceResponse response) {
+		if (response != null) {
+			Messages messages = response.getMessages();
+			boolean hasErrorMessages = messages.hasErrorMessages();
+			if (hasErrorMessages) {
+				for (Message message : messages.getErrorMessages()) {
+					LOGGER.error(MessageUtils.getSummaryMessage(message.getKey(), message.getInserts(), SimpleDomain.class));
+				}
+			}
+			assertFalse(hasErrorMessages);
+		}
+	}
+
+	public static void assertErrorMessagesInResponse(ServiceResponse response, int expectedNumErrorMessages) {
+		assertNotNull("Service response wrapper was unexpectedly null", response);
+		Messages messages = response.getMessages();
+		assertNotNull("Messages object on service response wrapper was unexpected null", messages);
+		assertEquals("An unexpected number of error messages were found", expectedNumErrorMessages,
+				messages.getErrorMessageCount());
+	}
 }

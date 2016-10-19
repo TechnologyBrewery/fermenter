e1942fa36e54c2e40525713e5a6a17daeee880d7
Ryan Ashcraft <ryan@cpointe-inc.com>
2016-10-19 10:37:13 -0400

    Added error message to help with HIbernateSessionFactory's that have not been initialized

--- stout/stout-hibernate/src/main/java/org/bitbucket/fermenter/stout/persist/hibernate/AbstractHibernateDaoImpl.java
+++ stout/stout-hibernate/src/main/java/org/bitbucket/fermenter/stout/persist/hibernate/AbstractHibernateDaoImpl.java
@@ -28,117 +28,124 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class AbstractHibernateDaoImpl<BO extends BusinessObject, PK extends PrimaryKey> implements Dao<BO, PK> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(AbstractHibernateDaoImpl.class);
-	private static final String DATABASE_EXCEPTION_KEY = "database.exception";
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractHibernateDaoImpl.class);
+    private static final String DATABASE_EXCEPTION_KEY = "database.exception";
 
-	protected Session getSession() {
-		SessionFactory hibernateSessionFactory = HibernateSessionFactoryManager.getInstance().getSessionFactory();
-		Session hibernateSession;
-		try {
-			hibernateSession = hibernateSessionFactory.getCurrentSession();
-		} catch (HibernateException e) {
-			hibernateSession = hibernateSessionFactory.openSession();
-		}
+    protected Session getSession() {
+        SessionFactory hibernateSessionFactory = HibernateSessionFactoryManager.getInstance().getSessionFactory();
 
-		return hibernateSession;
-	}
+        if (hibernateSessionFactory == null) {
+            LOG.warn("Your " + HibernateSessionFactoryManager.class.getSimpleName()
+                    + " has not been initialized. Calling init() on your behalf!");
+            HibernateSessionFactoryManager.getInstance().init();
+        }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public BO save(BO businessObject) {
-		try {
-			Session hibernateSession = getSession();
-			if (businessObject.getKey().getValue() == null) {
-				Serializable newlyPersistentObjectID = hibernateSession.save(businessObject);
-				businessObject.setKey(getNewPKWrapperForPersistentIDValue(newlyPersistentObjectID));
-			} else {
-				return (BO) hibernateSession.merge(businessObject);
-			}
-		} catch (HibernateException exception) {
-			return handleDataAccessException(businessObject, exception);
-		}
+        Session hibernateSession;
+        try {
+            hibernateSession = hibernateSessionFactory.getCurrentSession();
+        } catch (HibernateException e) {
+            hibernateSession = hibernateSessionFactory.openSession();
+        }
 
-		return businessObject;
-	}
+        return hibernateSession;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public BO delete(PK primaryKey) {
-		BO businessObject = findByPrimaryKey(primaryKey);
+    /**
+     * {@inheritDoc}
+     */
+    public BO save(BO businessObject) {
+        try {
+            Session hibernateSession = getSession();
+            if (businessObject.getKey().getValue() == null) {
+                Serializable newlyPersistentObjectID = hibernateSession.save(businessObject);
+                businessObject.setKey(getNewPKWrapperForPersistentIDValue(newlyPersistentObjectID));
+            } else {
+                return (BO) hibernateSession.merge(businessObject);
+            }
+        } catch (HibernateException exception) {
+            return handleDataAccessException(businessObject, exception);
+        }
 
-		if (businessObject != null) {
-			try {
-				getSession().delete(businessObject);
-			} catch (HibernateException exception) {
-				return handleDataAccessException(businessObject, exception);
-			}
-		} else {
-			LOG.warn("Could not find BO of type [" + getEntityName() + "] with PK [" + primaryKey.getValue()
-					+ "] to delete");
-		}
+        return businessObject;
+    }
 
-		return businessObject;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public BO delete(PK primaryKey) {
+        BO businessObject = findByPrimaryKey(primaryKey);
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public BO findByPrimaryKey(PK primaryKey) {
-		BO businessObject = null;
+        if (businessObject != null) {
+            try {
+                getSession().delete(businessObject);
+            } catch (HibernateException exception) {
+                return handleDataAccessException(businessObject, exception);
+            }
+        } else {
+            LOG.warn("Could not find BO of type [" + getEntityName() + "] with PK [" + primaryKey.getValue()
+                    + "] to delete");
+        }
 
-		try {
-			Serializable primaryKeyValue = primaryKey.getValue();
-			if (primaryKeyValue != null) {
-				businessObject = (BO) getSession().get(getBusinessObjectClazz(), primaryKey.getValue());
-			}
-		} catch (HibernateException exception) {
-			return handleDataAccessException(primaryKey, exception);
-		}
+        return businessObject;
+    }
 
-		return businessObject;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public BO findByPrimaryKey(PK primaryKey) {
+        BO businessObject = null;
 
-	/**
-	 * Answer a list of entities that match the criteria defined by a named query.
-	 * 
-	 * @param queryName
-	 *            Name of the query entity execute. Must be defined in a Hibernate mapping file.
-	 * @param paramNames
-	 *            Array of query parameter names. Must match param names in Hibernate mapping file.
-	 * @param paramValues
-	 *            Array of query parameter values
-	 * @param types
-	 *            Array of query parameter types
-	 * @return query results
-	 */
-	protected List<BO> query(String queryName, String[] paramNames, Object[] paramValues, Type[] paramTypes) {
-		List<BO> entities = null;
+        try {
+            Serializable primaryKeyValue = primaryKey.getValue();
+            if (primaryKeyValue != null) {
+                businessObject = (BO) getSession().get(getBusinessObjectClazz(), primaryKey.getValue());
+            }
+        } catch (HibernateException exception) {
+            return handleDataAccessException(primaryKey, exception);
+        }
 
-		try {
-			Query query = buildBaseQuery(queryName, paramNames, paramValues, paramTypes);
-			entities = (List<BO>) query.list();
-			
-		} catch (HibernateException exception) {
-			return Arrays.asList(handleDataAccessException(exception));
-		}
+        return businessObject;
+    }
 
-		return entities;
-	}
+    /**
+     * Answer a list of entities that match the criteria defined by a named query.
+     * 
+     * @param queryName
+     *            Name of the query entity execute. Must be defined in a Hibernate mapping file.
+     * @param paramNames
+     *            Array of query parameter names. Must match param names in Hibernate mapping file.
+     * @param paramValues
+     *            Array of query parameter values
+     * @param types
+     *            Array of query parameter types
+     * @return query results
+     */
+    protected List<BO> query(String queryName, String[] paramNames, Object[] paramValues, Type[] paramTypes) {
+        List<BO> entities = null;
+
+        try {
+            Query query = buildBaseQuery(queryName, paramNames, paramValues, paramTypes);
+            entities = (List<BO>) query.list();
+
+        } catch (HibernateException exception) {
+            return Arrays.asList(handleDataAccessException(exception));
+        }
+
+        return entities;
+    }
 
     private Query buildBaseQuery(String queryName, String[] paramNames, Object[] paramValues, Type[] paramTypes) {
         Session session = getSession();
         Query query = session.getNamedQuery(queryName);
         if (paramNames != null) {
-        	int length = paramNames.length;
-        	for (int i = 0; i < length; i++) {
-        		query.setParameter(paramNames[i], paramValues[i], paramTypes[i]);
-        	}
+            int length = paramNames.length;
+            for (int i = 0; i < length; i++) {
+                query.setParameter(paramNames[i], paramValues[i], paramTypes[i]);
+            }
         }
         return query;
     }
-	
+
     /**
      * Answer a list of entities that match the criteria defined by a named query with pagination.
      * 
@@ -153,10 +160,10 @@ public abstract class AbstractHibernateDaoImpl<BO extends BusinessObject, PK ext
      * @param firstResult
      *            First record to return for this given page of results
      * @param maxResults
-     *            Maximum number of results to return       
+     *            Maximum number of results to return
      * @return query results
      */
-    protected List<BO> query(String queryName, String[] paramNames, Object[] paramValues, Type[] paramTypes, 
+    protected List<BO> query(String queryName, String[] paramNames, Object[] paramValues, Type[] paramTypes,
             int firstResult, int maxResults) {
         List<BO> entities = null;
 
@@ -165,121 +172,121 @@ public abstract class AbstractHibernateDaoImpl<BO extends BusinessObject, PK ext
             query.setFirstResult(firstResult);
             query.setMaxResults(maxResults);
             entities = (List<BO>) query.list();
-            
+
         } catch (HibernateException exception) {
             return Arrays.asList(handleDataAccessException(exception));
         }
 
         return entities;
-    }	
-	
-	/**
-	 * Answer an update or delete that match the criteria defined by a named query.
-	 * 
-	 * @param statementName
-	 *            Name of the HQL statement to execute. Must be defined in a Hibernate mapping file.
-	 * @param paramNames
-	 *            Array of query parameter names. Must match param names in Hibernate mapping file.
-	 * @param paramValues
-	 *            Array of query parameter values
-	 * @param types
-	 *            Array of query parameter types
-	 * @return the number of updated or deleted records on successful execution or a negative number
-	 * 		   on a failed execution
-	 */
-	protected int updateOrDelete(String statementName, String[] paramNames, Object[] paramValues, Type[] paramTypes) {
-		int impactedRecords;
-		
-		try {
-			Query query = buildBaseQuery(statementName, paramNames, paramValues, paramTypes);
+    }
 
-			impactedRecords = query.executeUpdate();
-		} catch (HibernateException exception) {
-			impactedRecords = -1;
-		}
+    /**
+     * Answer an update or delete that match the criteria defined by a named query.
+     * 
+     * @param statementName
+     *            Name of the HQL statement to execute. Must be defined in a Hibernate mapping file.
+     * @param paramNames
+     *            Array of query parameter names. Must match param names in Hibernate mapping file.
+     * @param paramValues
+     *            Array of query parameter values
+     * @param types
+     *            Array of query parameter types
+     * @return the number of updated or deleted records on successful execution or a negative number on a failed
+     *         execution
+     */
+    protected int updateOrDelete(String statementName, String[] paramNames, Object[] paramValues, Type[] paramTypes) {
+        int impactedRecords;
 
-		return impactedRecords;
-	}	
+        try {
+            Query query = buildBaseQuery(statementName, paramNames, paramValues, paramTypes);
 
-	/**
-	 * Creates a new {@link PrimaryKey} wrapper based on the given ID value that was generated by Hibernate.
-	 * 
-	 * @param persistentIDValue
-	 *            ID that was created by Hibernate as a result of making a transient {@link BusinessObject} persistent.
-	 * @return wrapper {@link PrimaryKey} object that encapsulates the newly created {@link BusinessObject}'s ID.
-	 */
-	protected abstract PK getNewPKWrapperForPersistentIDValue(Serializable persistentIDValue);
+            impactedRecords = query.executeUpdate();
+        } catch (HibernateException exception) {
+            impactedRecords = -1;
+        }
 
-	/**
-	 * Returns the underlying {@link BusinessObject} implementation {@link Class} whose data access logic is managed by
-	 * this DAO.
-	 * 
-	 * @return
-	 */
-	protected abstract Class<BO> getBusinessObjectClazz();
+        return impactedRecords;
+    }
 
-	/**
-	 * Returns the logical name for the entity whose data access logic is managed by this DAO.
-	 * 
-	 * @return
-	 */
-	protected abstract String getEntityName();
+    /**
+     * Creates a new {@link PrimaryKey} wrapper based on the given ID value that was generated by Hibernate.
+     * 
+     * @param persistentIDValue
+     *            ID that was created by Hibernate as a result of making a transient {@link BusinessObject} persistent.
+     * @return wrapper {@link PrimaryKey} object that encapsulates the newly created {@link BusinessObject}'s ID.
+     */
+    protected abstract PK getNewPKWrapperForPersistentIDValue(Serializable persistentIDValue);
 
-	/**
-	 * Turn the database exception into a meaningful error message
-	 * 
-	 * @param pk
-	 * @param dae
-	 * @return
-	 */
-	private BO handleDataAccessException(PK pk, HibernateException dae) {
-		Throwable cause = dae.getCause();
+    /**
+     * Returns the underlying {@link BusinessObject} implementation {@link Class} whose data access logic is managed by
+     * this DAO.
+     * 
+     * @return
+     */
+    protected abstract Class<BO> getBusinessObjectClazz();
 
-		if (cause != null && cause instanceof ObjectDeletedException) {
-			// Object was deleted - return null
-			return null;
-		} else if (cause != null && cause instanceof ObjectNotFoundException) {
-			// Object not found - return null
-			return null;
-		}
+    /**
+     * Returns the logical name for the entity whose data access logic is managed by this DAO.
+     * 
+     * @return
+     */
+    protected abstract String getEntityName();
 
-		AbstractBusinessObjectFactoryInterface factory = (AbstractBusinessObjectFactoryInterface) FactoryManager
-				.createFactory(FactoryManager.BUSINESS_OBJECT, pk.getClass());
-		BusinessObject bo = factory.createBusinessObject(pk.getEntityName());
-		bo.setKey(pk);
-		return handleDataAccessException((BO) bo, dae);
-	}
+    /**
+     * Turn the database exception into a meaningful error message
+     * 
+     * @param pk
+     * @param dae
+     * @return
+     */
+    private BO handleDataAccessException(PK pk, HibernateException dae) {
+        Throwable cause = dae.getCause();
 
-	/**
-	 * Turn the database exception into a meaningful error message
-	 * 
-	 * @param dae
-	 * @return
-	 */
-	protected BO handleDataAccessException(HibernateException dae) {
-		AbstractBusinessObjectFactoryInterface factory = (AbstractBusinessObjectFactoryInterface) FactoryManager
-				.createFactory(FactoryManager.BUSINESS_OBJECT, getClass());
-		BusinessObject bo = factory.createBusinessObject(getEntityName());
-		return handleDataAccessException((BO) bo, dae);
-	}
+        if (cause != null && cause instanceof ObjectDeletedException) {
+            // Object was deleted - return null
+            return null;
+        } else if (cause != null && cause instanceof ObjectNotFoundException) {
+            // Object not found - return null
+            return null;
+        }
 
-	/**
-	 * Turn the database exception into a meaningful error message
-	 * 
-	 * @param entity
-	 * @param dae
-	 * @return
-	 */
-	protected BO handleDataAccessException(BO businessObject, HibernateException dae) {
-		if (LOG.isDebugEnabled()) {
-			LOG.error("A problem was encountered interacting with the database:", dae);
-		}
-		Message message = MessageFactory.createMessage();
-		message.setKey(DATABASE_EXCEPTION_KEY);
-		message.addInsert(dae.getMessage());
-		message.setSeverity(Severity.ERROR);
-		MessageManager.addMessage(message);
+        AbstractBusinessObjectFactoryInterface factory = (AbstractBusinessObjectFactoryInterface) FactoryManager
+                .createFactory(FactoryManager.BUSINESS_OBJECT, pk.getClass());
+        BusinessObject bo = factory.createBusinessObject(pk.getEntityName());
+        bo.setKey(pk);
+        return handleDataAccessException((BO) bo, dae);
+    }
 
-		return businessObject;
-	}
+    /**
+     * Turn the database exception into a meaningful error message
+     * 
+     * @param dae
+     * @return
+     */
+    protected BO handleDataAccessException(HibernateException dae) {
+        AbstractBusinessObjectFactoryInterface factory = (AbstractBusinessObjectFactoryInterface) FactoryManager
+                .createFactory(FactoryManager.BUSINESS_OBJECT, getClass());
+        BusinessObject bo = factory.createBusinessObject(getEntityName());
+        return handleDataAccessException((BO) bo, dae);
+    }
+
+    /**
+     * Turn the database exception into a meaningful error message
+     * 
+     * @param entity
+     * @param dae
+     * @return
+     */
+    protected BO handleDataAccessException(BO businessObject, HibernateException dae) {
+        if (LOG.isDebugEnabled()) {
+            LOG.error("A problem was encountered interacting with the database:", dae);
+        }
+        Message message = MessageFactory.createMessage();
+        message.setKey(DATABASE_EXCEPTION_KEY);
+        message.addInsert(dae.getMessage());
+        message.setSeverity(Severity.ERROR);
+        MessageManager.addMessage(message);
+
+        return businessObject;
+    }
 }

933a6f0446d01ba41d21d0a3f62da9203a947938
Eric Konieczny <ekonieczny@cpointe-inc.com>
2017-01-18 16:06:38 -0500

    Fixed bug involving wrong listener-class being generated into web.xml Added generation of entity maintenance service interfaces to facilitate usage of RestEasy client proxies

--- stout/stout-cookbook/stout-cookbook-domain/src/test/java/org/bitbucket/askllc/fermenter/cookbook/domain/AbstractArquillianTestSupport.java
+++ stout/stout-cookbook/stout-cookbook-domain/src/test/java/org/bitbucket/askllc/fermenter/cookbook/domain/AbstractArquillianTestSupport.java
@@ -2,6 +2,9 @@ package org.bitbucket.askllc.fermenter.cookbook.domain;
 
 import java.io.File;
 
+import javax.ws.rs.client.WebTarget;
+
+import org.bitbucket.askllc.fermenter.cookbook.domain.service.rest.JacksonObjectMapperResteasyProvider;
 import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.Filters;
@@ -14,9 +17,9 @@ import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 @ArquillianSuiteDeployment
 public abstract class AbstractArquillianTestSupport {
 
-	@Deployment
+	@Deployment()
 	public static WebArchive createDeployment() {
-		File[] mavenDependencies = Maven.configureResolver().loadPomFromFile("./pom.xml", "integration-test")
+		File[] mavenDependencies = Maven.configureResolver().loadPomFromFile("pom.xml", "integration-test")
 				.importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
 
 		WebArchive war = ShrinkWrap.create(WebArchive.class, "cookbook-domain.war");
@@ -29,7 +32,20 @@ public abstract class AbstractArquillianTestSupport {
 				"WEB-INF/classes", Filters.includeAll());
 		war.addAsWebInfResource("application-context.xml");
 		war.addAsWebInfResource("h2-tomcat-ds-context.xml", "context.xml");
-		war.addAsWebInfResource("web.xml");
+		war.addAsWebResource("log4j2.xml");
+		war.setWebXML("web.xml");
 		return war;
+	}
+
+	/**
+	 * Registers the given JAX-RS {@link WebTarget} (typically injected into a test via
+	 * {@link ArquillianResteasyResource}) with the generated {@link JacksonObjectMapperResteasyProvider} such that the
+	 * (de)serialization of generated business objects is transparently supported.
+	 * 
+	 * @param webTarget
+	 * @return
+	 */
+	protected WebTarget initWebTarget(WebTarget webTarget) {
+		return webTarget.register(JacksonObjectMapperResteasyProvider.class);
 	}
 }
--- stout/stout-cookbook/stout-cookbook-domain/src/test/java/org/bitbucket/askllc/fermenter/cookbook/domain/SimpleDomainManagerIT.java
+++ stout/stout-cookbook/stout-cookbook-domain/src/test/java/org/bitbucket/askllc/fermenter/cookbook/domain/SimpleDomainManagerIT.java
@@ -4,16 +4,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.bitbucket.askllc.fermenter.cookbook.domain.bizobj.SimpleDomainBO;
-import org.bitbucket.askllc.fermenter.cookbook.domain.service.impl.SimpleDomainMaintenanceService;
+import org.bitbucket.askllc.fermenter.cookbook.domain.service.rest.JacksonObjectMapperResteasyProvider;
+import org.bitbucket.askllc.fermenter.cookbook.domain.service.rest.SimpleDomainMaintenanceService;
 import org.bitbucket.fermenter.stout.service.ValueServiceResponse;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class SimpleDomainManagerIT extends AbstractArquillianTestSupport {
+
+	@Before
+	public void setup() {
+		ResteasyProviderFactory factory = ResteasyProviderFactory.getInstance();
+		factory.registerProvider(JacksonObjectMapperResteasyProvider.class);
+		RegisterBuiltin.register(factory);
+	}
 
 	@Test
 	@RunAsClient
@@ -27,4 +38,5 @@ public class SimpleDomainManagerIT extends AbstractArquillianTestSupport {
 		assertEquals(simpleDomain.getKey(), retrievedSimpleDomain.getKey());
 		assertNotNull(retrievedSimpleDomain.getUpdatedAt());
 	}
+
 }

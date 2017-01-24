d54839891cacbafa1c79fedef1d002888eea25bb
Ryan Ashcraft <ryan@cpointe-inc.com>
2017-01-24 13:39:56 -0500

    #23: This time the correct change set to resolve business services being picked up by RestEasy

--- stout/stout-cookbook/stout-cookbook-domain/src/main/java/org/bitbucket/askllc/fermenter/cookbook/domain/service/rest/SimpleDomainManagerService.java
+++ stout/stout-cookbook/stout-cookbook-domain/src/main/java/org/bitbucket/askllc/fermenter/cookbook/domain/service/rest/SimpleDomainManagerService.java
@@ -15,6 +15,7 @@ import javax.ws.rs.core.Response;
  * 
  *      GENERATED STUB CODE - PLEASE *DO* MODIFY
  */
+@Path("/")
 public interface SimpleDomainManagerService extends SimpleDomainManagerBaseService {
 
 	// Developers should add any service operations here that cannot be defined via the PIM

ee313d7111b4d4f1caecf9ae39a562711bbc2be9
Mindy Lair <mlair@cpointe-inc.com>
2017-12-07 15:05:42 -0500

    [FER-61] beefing up logging

--- stout/stout-spring/src/main/java/org/bitbucket/fermenter/stout/bizobj/BasePersistentSpringBO.java
+++ stout/stout-spring/src/main/java/org/bitbucket/fermenter/stout/bizobj/BasePersistentSpringBO.java
@@ -1,9 +1,12 @@
 package org.bitbucket.fermenter.stout.bizobj;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.UUID;
 
+import org.bitbucket.fermenter.stout.messages.Message;
 import org.bitbucket.fermenter.stout.messages.MessageManager;
+import org.bitbucket.fermenter.stout.messages.MessageUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -13,36 +16,47 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @param <PK>
  * @param <BO>
  */
-public abstract class BasePersistentSpringBO<PK extends Serializable, BO, JPA extends JpaRepository<BO, PK>> extends BaseSpringBO<BO> implements PersistentBusinessObject<PK, BO> {
+public abstract class BasePersistentSpringBO<PK extends Serializable, BO, JPA extends JpaRepository<BO, PK>>
+        extends BaseSpringBO<BO> implements PersistentBusinessObject<PK, BO> {
 
     /**
      * Transient ID that is not persistent but guaranteed to be non-null during an object's lifecycle and thereby usable
      * in hashCode() and equals() methods as a fallback for scenarios where an entity may not have a non-null PK value.
      */
     protected final UUID internalTransientID = UUID.randomUUID();
-    
-	/**
-	 * {@inheritDoc}
-	 */
-	public BO save() {
-		defaultValues();
-		preValidate();
-		validate();
-		if (!MessageManager.hasErrorMessages()) {
-			postValidate();
-			BO persistedBizObj = getRepository().save((BO) this);
-			postSave();
-			return persistedBizObj;
-		} else {
-			if (getLogger().isWarnEnabled()) {
-				getLogger().warn("Attempt to save BO of type [" + this.getClass() + "] with PK = [" + this.getKey()
-						+ "] was ignored due to collected errors");
-			}
-			return (BO) this;
-		}
 
-	}
-	
+    /**
+     * {@inheritDoc}
+     */
+    public BO save() {
+        defaultValues();
+        preValidate();
+        validate();
+        if (!MessageManager.hasErrorMessages()) {
+            postValidate();
+            BO persistedBizObj = getRepository().save((BO) this);
+            postSave();
+            return persistedBizObj;
+        } else {
+            if (getLogger().isDebugEnabled()) {
+                Collection<Message> messages = MessageManager.getMessages().getErrorMessages();
+                for (Message message : messages) {
+                    getLogger().debug("Encountered the following error when trying to save persistent object: "
+                            + this.toString() + "\n\t"
+                            + MessageUtils.getSummaryMessage(message.getKey(), message.getInserts(), this.getClass()));
+                }
+
+            }
+            if (getLogger().isWarnEnabled()) {
+                getLogger().warn("Attempt to save BO of type [" + this.getClass() + "] with PK = [" + this.getKey()
+                        + "] was ignored due to collected errors");
+            }
+
+            return (BO) this;
+        }
+
+    }
+
     /**
      * Lifecycle method that is invoked before this business object's pre-save validation occurs to support defaulting.
      */
@@ -50,47 +64,47 @@ public abstract class BasePersistentSpringBO<PK extends Serializable, BO, JPA ex
 
     }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void delete() {
-		getRepository().delete((BO) this);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public void delete() {
+        getRepository().delete((BO) this);
+    }
 
-	protected abstract JPA getRepository();
+    protected abstract JPA getRepository();
 
-	/**
-	 * Lifecycle method that is invoked after this business object has been persisted. Developers may override this
-	 * method in concrete business implementations to do things like broadcasting a notification after an object has
-	 * been successfully saved.
-	 */
-	protected void postSave() {
+    /**
+     * Lifecycle method that is invoked after this business object has been persisted. Developers may override this
+     * method in concrete business implementations to do things like broadcasting a notification after an object has
+     * been successfully saved.
+     */
+    protected void postSave() {
 
-	}
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		boolean areEqual = false;
+    @Override
+    public boolean equals(Object o) {
+        boolean areEqual = false;
 
-		try {
-			BasePersistentSpringBO thatBizObj = (BasePersistentSpringBO) o;
+        try {
+            BasePersistentSpringBO thatBizObj = (BasePersistentSpringBO) o;
 
-			PK thisPk = getKey();
-			PK thatPk = (PK) (thatBizObj != null ? thatBizObj.getKey() : null);
-			if (thatBizObj != null && thisPk == null && thatPk == null) {
-				return this.internalTransientID == thatBizObj.internalTransientID;
-			} else if (thisPk == thatPk || (thisPk.equals(thatPk))) {
-				areEqual = true;
-			}
-		} catch (ClassCastException ex) {
-			areEqual = false;
-		}
+            PK thisPk = getKey();
+            PK thatPk = (PK) (thatBizObj != null ? thatBizObj.getKey() : null);
+            if (thatBizObj != null && thisPk == null && thatPk == null) {
+                return this.internalTransientID == thatBizObj.internalTransientID;
+            } else if (thisPk == thatPk || (thisPk.equals(thatPk))) {
+                areEqual = true;
+            }
+        } catch (ClassCastException ex) {
+            areEqual = false;
+        }
 
-		return areEqual;
-	}
+        return areEqual;
+    }
 
-	@Override
-	public int hashCode() {
-		return (getKey() == null) ? internalTransientID.hashCode() : getKey().hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return (getKey() == null) ? internalTransientID.hashCode() : getKey().hashCode();
+    }
 }

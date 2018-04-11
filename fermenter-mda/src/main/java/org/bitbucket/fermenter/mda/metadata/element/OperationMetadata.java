8034715bd609ef65b164a385fd03d72181a97fad
Andrew Walters <awalters@cpointe-inc.com>
2018-04-11 15:15:00 -0400

    FER-63 Small cleanups to address comments in PR, and also remove me getting ahead of myself trying to implement another ticket alongside this one.  Added some additional tests via including more compression tags in the SimpleDomainManager.xml.

--- fermenter-mda/src/main/java/org/bitbucket/fermenter/mda/metadata/element/OperationMetadata.java
+++ fermenter-mda/src/main/java/org/bitbucket/fermenter/mda/metadata/element/OperationMetadata.java
@@ -137,7 +137,7 @@ public class OperationMetadata extends MetadataElement implements Operation {
 	 * @return Whether this data will be compressed using GZIP.
 	 */
     public boolean isCompressedWithGzip() {
-        return (this.compressWithGzip != null && this.compressWithGzip.equalsIgnoreCase("true"));
+        return (this.compressWithGzip != null && "true".equalsIgnoreCase(compressWithGzip));
     }
 
     /**

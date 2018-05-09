cd32c5da6caf68af1612297aec9b89c415f89d39
Ryan Ashcraft <ryan@cpointe-inc.com>
2018-05-09 17:55:14 -0400

    FER-107: updated to spit out an error when the class is instantiated should a mismatch be present.  Only happens once, then short-circuits thereafter.

---
+++ stout/stout-cookbook/stout-cookbook-domain/src/main/java/org/bitbucket/askllc/fermenter/cookbook/domain/bizobj/TableNameMismatchBO.java
@@ -0,0 +1,37 @@
+package org.bitbucket.askllc.fermenter.cookbook.domain.bizobj;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.bitbucket.fermenter.stout.util.SpringAutowiringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Business object for the TableNameMismatch entity.
+ * @see org.bitbucket.askllc.fermenter.cookbook.domain.bizobj.TableNameMismatchBaseBO
+ *
+ * GENERATED STUB CODE - PLEASE *DO* MODIFY
+ */
+@Entity
+@Table(name="OLD_TABLE_NAME")
+public class TableNameMismatchBO extends TableNameMismatchBaseBO {
+	
+	public TableNameMismatchBO() {
+		super();
+		SpringAutowiringUtil.autowireBizObj(this);
+	}
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TableNameMismatchBO.class);
+	
+	@Override
+	protected Logger getLogger() {
+		return LOGGER;
+	}
+
+	@Override
+	protected void complexValidation() {
+
+	}
+	
+}

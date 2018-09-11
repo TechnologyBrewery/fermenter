fc25e63d73638577e897ceea13ba317501b0040b
Ryan Ashcraft <ryan@cpointe-inc.com>
2018-09-11 13:07:29 -0400

    FER-129 implemented both maxLength and minLength validation steps

---
+++ stout/stout-cookbook/stout-cookbook-domain/src/test/java/org/bitbucket/askllc/fermenter/cookbook/domain/ValidationSteps.java
@@ -0,0 +1,83 @@
+package org.bitbucket.askllc.fermenter.cookbook.domain;
+
+import static org.junit.Assert.assertFalse;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.bitbucket.askllc.fermenter.cookbook.domain.bizobj.ValidationExampleBO;
+import org.bitbucket.fermenter.stout.messages.MessageManager;
+import org.bitbucket.fermenter.stout.messages.MessageManagerInitializationDelegate;
+import org.bitbucket.fermenter.stout.test.MessageTestUtils;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+@ContextConfiguration({ "classpath:stout-cookbook-domain-application-context.xml",
+		"classpath:h2-spring-ds-context.xml" })
+@Transactional
+public class ValidationSteps {
+
+	private String userString;
+	private ValidationExampleBO example;
+	private ValidationExampleBO example2;
+
+	@After("@fieldValidation")
+	public void cleanupMsgMgr() throws Exception {
+		MessageManagerInitializationDelegate.cleanupMessageManager();
+	}
+
+	@Given("^a \"([^\"]*)\" to validate against the validation example string example field$")
+	public void a_to_validate_against_the_validation_example_string_example_field(String value) throws Throwable {
+		this.userString = value;
+
+		example = new ValidationExampleBO();
+		example.setStringExample(userString);
+		example.setRequiredField(RandomStringUtils.randomAlphanumeric(10));
+
+	}
+
+	@When("^field level validation is performed on that value$")
+	public void field_level_validation_is_performed_on_that_value() throws Throwable {
+
+		example.validate();
+
+	}
+
+	@Then("^the validation returns no errors$")
+	public void the_validation_returns_no_errors() throws Throwable {
+		MessageTestUtils.logErrors("Error Messages", MessageManager.getMessages(),
+				org.bitbucket.fermenter.stout.messages.Messages.class);
+		assertFalse("Should not have encountered messages!", MessageManager.hasErrorMessages());
+
+	}
+
+	@Given("^a \"([^\"]*)\" to validate against the invalidation example string example field$")
+	public void a_to_validate_against_the_invalidation_example_string_example_field(String value2) throws Throwable {
+		this.userString = value2;
+
+		example2 = new ValidationExampleBO();
+		example2.setStringExample(userString);
+		example2.setRequiredField(RandomStringUtils.randomAlphanumeric(3));
+
+	}
+
+	@When("^field level invalidation is performed on that value$")
+	public void field_level_invalidation_is_performed_on_that_value() throws Throwable {
+
+		example2.validate();
+
+	}
+
+	@Then("^the validation return errors$")
+	public void the_validation_return_errors() throws Throwable {
+
+		MessageTestUtils.logErrors("Error Messages", MessageManager.getMessages(),
+				org.bitbucket.fermenter.stout.messages.Messages.class);
+		assertFalse("Should have encountered messages!", MessageManager.hasErrorMessages());
+
+	}
+
+}

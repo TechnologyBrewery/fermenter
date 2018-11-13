2871407c6075d6ba17323f3a78c22e7ab228aacb
Ethan Chun <echun@cpointe-inc.com>
2018-11-13 15:38:20 -0500

    FER-142: code review feedback (+1 squashed commit) Squashed commits: [ddfc694] FER-142: moved MockRequestScope into stout-test, updated the pom.xml file in stout-test, and made a few minor fixes to the package

--- stout/stout-core/src/test/java/org/bitbucket/fermenter/stout/exception/ExceptionHandlerTestSpecifications.java
+++ stout/stout-core/src/test/java/org/bitbucket/fermenter/stout/exception/ExceptionHandlerTestSpecifications.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
  * Test harness to run Gherkin Business Driven Development specifications.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/specifications", plugin="json:target/cucumber-html-reports/cucumber.json")
+@CucumberOptions(features = "src/test/resources/specifications", plugin = "json:target/cucumber-html-reports/cucumber.json")
 public class ExceptionHandlerTestSpecifications {
 
 }

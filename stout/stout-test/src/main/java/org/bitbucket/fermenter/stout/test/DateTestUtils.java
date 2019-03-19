795a3afc59c89f00b32e47e22b19b8f94ce2868e
Mindy Lair <mlair@cpointe-inc.com>
2019-03-19 06:11:01 -0400

    [FER-196] Updating dateformatter to use ThreadLocal

--- stout/stout-test/src/main/java/org/bitbucket/fermenter/stout/test/DateTestUtils.java
+++ stout/stout-test/src/main/java/org/bitbucket/fermenter/stout/test/DateTestUtils.java
@@ -7,14 +7,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.commons.lang3.StringUtils;
 
 public final class DateTestUtils {
 	
-	private static final DateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
+    private static ThreadLocal<DateFormat> threadLocalDateFormat = ThreadLocal
+            .withInitial(() -> new SimpleDateFormat("yyyy/MM/dd"));
 
     private DateTestUtils() {
 
@@ -31,6 +32,7 @@ public final class DateTestUtils {
      */
     public static Date convertToDate(String dateStr) {
         Date date = null;
+        DateFormat formatter = threadLocalDateFormat.get();
         if (StringUtils.equalsIgnoreCase("today", StringUtils.trim(dateStr))) {
             date = getRelativeDate(0);
         } else if (StringUtils.equalsIgnoreCase("tomorrow", StringUtils.trim(dateStr))) {
@@ -40,7 +42,7 @@ public final class DateTestUtils {
         } else if (StringUtils.isNotBlank(dateStr)) {
             try {
                 if (StringUtils.isNotEmpty(dateStr)) {
-                    date = FORMATTER.parse(dateStr);
+                    date = formatter.parse(dateStr);
                 }
             } catch (ParseException e) {
                 LOGGER.error("Error parsing date string: " + dateStr, e.getMessage());

10d09482d040c8ac6a6310242483b3145a6f4034
Ryan Ashcraft <ryan@cpointe-inc.com>
2020-01-28 16:56:00 -0500

    FER-252: Extend datasouce and jpa properties overrides to co-deployed domains

--- stout/stout-spring/src/main/java/org/bitbucket/fermenter/stout/util/KrauseningBasedSpringConfig.java
+++ stout/stout-spring/src/main/java/org/bitbucket/fermenter/stout/util/KrauseningBasedSpringConfig.java
@@ -12,51 +12,60 @@ import org.bitbucket.fermenter.stout.exception.UnrecoverableException;
 import org.bitbucket.krausening.Krausening;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Exposes several beans that are wired up and initialized with the help of {@link Krausening} properties for usage
  * within Spring's application context.
  */
-@Configuration
 public class KrauseningBasedSpringConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(KrauseningBasedSpringConfig.class);
 
-    private String jpaPropertiesFileName;
-    private String dataSourcePropertiesFileName;
+    protected String jpaPropertiesFileName;
+    protected String dataSourcePropertiesFileName;
 
     protected static final String DEFAULT_JPA_PROPERTIES = "jpa.properties";
     protected static final String DEFAULT_DATA_SOURCE_PROPERTIES = "data-source.properties";
 
+    /**
+     * Configures this class w/ the default jpa.properties and data-source.properties Krausening files.
+     */
     public KrauseningBasedSpringConfig() {
         this(DEFAULT_JPA_PROPERTIES, DEFAULT_DATA_SOURCE_PROPERTIES);
     }
 
+    /**
+     * Allows you to override the krausening profile file names used to configure JPA and the data source for this
+     * domain.
+     */
     public KrauseningBasedSpringConfig(String jpaPropertiesFileName, String dataSourcePropertiesFileName) {
         this.jpaPropertiesFileName = jpaPropertiesFileName;
         this.dataSourcePropertiesFileName = dataSourcePropertiesFileName;
     }
 
-    @Bean
+    /**
+     * Configures the datasource used for this domain.
+     * 
+     * @return a datasource
+     */
     public DataSource krauseningDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         Properties dataSourceProps = Krausening.getInstance().getProperties(this.dataSourcePropertiesFileName);
         if (dataSourceProps == null) {
             logger.error(
                     "Could not find properties for {}!  You emf bean will not be able to load without these properties! Using defaults instead...",
-                    this.dataSourcePropertiesFileName); 
+                    this.dataSourcePropertiesFileName);
             dataSourceProps = new Properties();
         }
-        
-        DataSourceConfig config = KrauseningConfigFactory.create(DataSourceConfig.class, dataSourcePropertiesFileName, System.getProperties());
+
+        DataSourceConfig config = KrauseningConfigFactory.create(DataSourceConfig.class, dataSourcePropertiesFileName,
+                System.getProperties());
         String interleavedUrl = config.getUrl();
 
         if (interleavedUrl != null) {
             dataSourceProps.put("url", interleavedUrl);
         }
-        
+
         for (String propName : dataSourceProps.stringPropertyNames()) {
             try {
                 BeanUtils.setProperty(dataSource, propName, dataSourceProps.getProperty(propName));
@@ -66,11 +75,13 @@ public class KrauseningBasedSpringConfig {
             }
         }
 
-
         return dataSource;
     }
 
-    @Bean
+    /**
+     * Configures the JPA properties used for this domain.
+     * @return properties for JPA
+     */     
     public Properties krauseningJpaProperties() {
         return Krausening.getInstance().getProperties(this.jpaPropertiesFileName);
     }

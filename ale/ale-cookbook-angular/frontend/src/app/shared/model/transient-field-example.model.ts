0b3e275273d8f2636a185a159771c705ab1b5d50
Ryan Ashcraft <ryan@cpointe-inc.com>
2020-06-16 15:07:04 -0400

    FER-276: minor tweak to allow column to not be specified when a field is transient

---
+++ ale/ale-cookbook-angular/frontend/src/app/shared/model/transient-field-example.model.ts
@@ -0,0 +1,13 @@
+import { TransientFieldExampleBase } from '../../generated/model-base/transient-field-example-base.model';
+
+/*******************************************************
+* BO for Angular frontend for TransientFieldExample
+*
+* Generated Code - DO MODIFY
+* Template = entity.model.ts.vm
+*******************************************************/
+export class TransientFieldExample extends TransientFieldExampleBase {
+  constructor(transientFieldExample?: TransientFieldExample) {
+    super(transientFieldExample);
+  }
+}
---
+++ stout/stout-cookbook/stout-cookbook-domain-client/src/main/java/org/bitbucket/askllc/fermenter/cookbook/domain/transfer/TransientFieldExample.java
@@ -0,0 +1,11 @@
+package org.bitbucket.askllc.fermenter.cookbook.domain.transfer;
+
+
+/**
+ * Transfer object for the TransientFieldExample entity.
+ * @see org.bitbucket.askllc.fermenter.cookbook.domain.transfer.TransientFieldExampleBase
+ *
+ * GENERATED STUB CODE - PLEASE *DO* MODIFY
+ */
+public class TransientFieldExample extends TransientFieldExampleBase {
+}

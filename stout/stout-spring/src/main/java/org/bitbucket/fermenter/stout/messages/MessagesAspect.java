a2af1108ee3c99cdd438ee291f2cd7110b4475da
Haumaira Safi <hsafi@Haumairas-MBP.fios-router.home>
2020-12-22 12:26:05 -0500

    [FER-307] PR feedback

--- stout/stout-spring/src/main/java/org/bitbucket/fermenter/stout/messages/MessagesAspect.java
+++ stout/stout-spring/src/main/java/org/bitbucket/fermenter/stout/messages/MessagesAspect.java
@@ -29,9 +29,10 @@ public class MessagesAspect {
 
         // See if we're running in an async thread and have set up the local
         // messages already
-        if (!isRequestScopeAvailable() && MessageManager.isCreatedLocally()) {
+        boolean isRequestScopeAvailable = isRequestScopeAvailable();
+        if (!isRequestScopeAvailable && MessageManager.isCreatedLocally()) {
             // Don't reinitialize the messages if we're in an async thread and using local messages
-        } else  if (!isRequestScopeAvailable() && !MessageManager.isCreatedLocally()) {
+        } else  if (!isRequestScopeAvailable && !MessageManager.isCreatedLocally()) {
             MessageManager.initialize(new NotSpringManagedDefaultMessages());
             MessageManager.setIsCreatedLocally();
         } else {

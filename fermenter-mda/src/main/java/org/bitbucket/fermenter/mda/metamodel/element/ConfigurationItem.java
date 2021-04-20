7b0e6f1b50eab72599a6702a029344ce27c89e50
Ryan Ashcraft <ryan@cpointe-inc.com>
2021-04-20 15:27:21 -0400

    FER-324: :sparkles: Add reusable configuration item metamodels for general purpose use across any ModelRepositoryInstance

---
+++ fermenter-mda/src/main/java/org/bitbucket/fermenter/mda/metamodel/element/ConfigurationItem.java
@@ -0,0 +1,22 @@
+package org.bitbucket.fermenter.mda.metamodel.element;
+
+/**
+ * Defines the contract for a configuration item.
+ */
+public interface ConfigurationItem extends Validatable {
+
+    /**
+     * Returns the configuration key.
+     * 
+     * @return configuration key
+     */
+    String getKey();
+
+    /**
+     * Returns the configuration value.
+     * 
+     * @return configuration value
+     */
+    String getValue();
+
+}
---
+++ fermenter-mda/src/main/java/org/bitbucket/fermenter/mda/metamodel/element/ConfigurationItemElement.java
@@ -0,0 +1,72 @@
+package org.bitbucket.fermenter.mda.metamodel.element;
+
+import org.apache.commons.lang3.StringUtils;
+import org.bitbucket.fermenter.mda.util.MessageTracker;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/**
+ * Represents a configuration item.
+ */
+@JsonPropertyOrder({ "key", "value" })
+public class ConfigurationItemElement implements ConfigurationItem {
+
+    protected static MessageTracker messageTracker = MessageTracker.getInstance();
+
+    @JsonInclude(Include.NON_NULL)
+    private String key;
+
+    @JsonInclude(Include.NON_NULL)
+    private String value;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Sets the configuration item key.
+     * 
+     * @param key
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the configuration item value.
+     * 
+     * @param key
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void validate() {
+        if (StringUtils.isBlank(getKey())) {
+            messageTracker.addErrorMessage("A key is required for each configuration item!");
+        }
+
+        if (StringUtils.isBlank(getValue())) {
+            messageTracker.addErrorMessage("A value is required for each configuration item!");
+        }
+    }
+
+}

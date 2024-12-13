504014bd83b7522fb107ca4daf36c7879272b6b1
Carter Cundiff <cundiff_carter@bah.com>
2024-12-13 09:30:21 -0500

    #78 Added functionality for accessing the current ModelInstanceRepository implementation outside of the GenerationContext

--- fermenter-mda/src/main/java/org/technologybrewery/fermenter/mda/metamodel/ModelInstanceRepositoryManager.java
+++ fermenter-mda/src/main/java/org/technologybrewery/fermenter/mda/metamodel/ModelInstanceRepositoryManager.java
@@ -10,19 +10,43 @@ public final class ModelInstanceRepositoryManager {
 
     private static ThreadLocal<Map<String, Object>> threadBoundInstance = ThreadLocal.withInitial(HashMap::new);
 
+    private static final String ABSTRACT_MODEL_INSTANCE_REPOSITORY_CLASS_NAME = 
+                                "org.technologybrewery.fermenter.mda.metamodel.AbstractModelInstanceRepository";
+
     private ModelInstanceRepositoryManager() {
         // prevent private instantiation of all static class
     }
 
     /**
-     * Adds a repository. Only one repository of each type will be kept.
+     * Sets a repository as the repository implementation for its own class and any super 
+     * classes if present. This enables downstream projects to safely extend upstream 
+     * {@link ModelInstanceRepository}'s without breaking existing calls to getRepository().
      * 
-     * @param respository
+     * For example, given the classes:
+     *  MIR_A implements AbstractModelInstanceRepository
+     *  MIR_B extends MIR_A
+     *  MIR_C extends MIR_B
+     * 
+     * When the repository is set using setRepository(MIR_C). Then following calls to 
+     * getRepository would all return the MIR_C instance:
+     *  getMetamodelRepository(MIR_A.class) -> MIR_C
+     *  getMetamodelRepository(MIR_B.class) -> MIR_C
+     *  getMetamodelRepository(MIR_C.class) -> MIR_C
+     * 
+     * @param repository
      *            repository to add
      */
-    public static void setRepository(ModelInstanceRepository respository) {
+    public static void setRepository(ModelInstanceRepository repository) {
         Map<String, Object> instanceMap = threadBoundInstance.get();
-        instanceMap.put(respository.getClass().toString(), respository);
+
+        instanceMap.put(repository.getClass().toString(), repository);
+        Class<?> repositorySuperClass = repository.getClass().getSuperclass();
+
+        // Add an entry for all superclasses of the repository
+        while (!repositorySuperClass.getName().equals(ABSTRACT_MODEL_INSTANCE_REPOSITORY_CLASS_NAME)) {
+            instanceMap.put(repositorySuperClass.toString(), repository);
+            repositorySuperClass = repositorySuperClass.getSuperclass();
+        }
     }
 
     /**
